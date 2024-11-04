27b739b15769906b874f22d3e38ee5d2840deea2
evcreton <evan.creton37@gmail.com>
2024-11-04 09:05:01 +0100

    Arborescence

---
+++ src/test/java/TestPasswordLenght.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+
+public class TestPasswordLenght {
+    @Test
+    public void TestInstance() {
+
+    }
+}
