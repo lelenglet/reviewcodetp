5b2cde8b550fb864c9cb9e53bb8b004a5675004e
lelenglet <lenglet.lea03@gmail.com>
2024-11-04 09:12:45 +0100

    javadoc init

--- src/main/java/fr/isima/codereview/tp1/AwesomePasswordChecker.java
+++ src/main/java/fr/isima/codereview/tp1/AwesomePasswordChecker.java
@@ -10,35 +10,50 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class AwesomePasswordChecker {
 
   private static AwesomePasswordChecker instance;
 
   private final List<double[]> clusterCenters = new ArrayList<>();
 
+  /**
+   * 
+   * @param file
+   * @return
+   * @throws IOException
+   */
   public static AwesomePasswordChecker getInstance(File file) throws IOException {
     if (instance == null) {
-          instance = new AwesomePasswordChecker(new FileInputStream(file));
+      instance = new AwesomePasswordChecker(new FileInputStream(file));
     }
     return instance;
   }
-  
+
+  /**
+   * 
+   * @return
+   * @throws IOException
+   */
   public static AwesomePasswordChecker getInstance() throws IOException {
     if (instance == null) {
       InputStream is = AwesomePasswordChecker.class.getClassLoader().getResourceAsStream("cluster_centers_HAC_aff.csv");
       instance = new AwesomePasswordChecker(is);
     }
-      return instance;
+    return instance;
   }
-      
+
+  /**
+   * 
+   * @param is
+   * @throws IOException
+   */
   private AwesomePasswordChecker(InputStream is) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(is));
-  String line;
-    while((line = br.readLine()) != null){
+    String line;
+    while ((line = br.readLine()) != null) {
       String[] values = line.split(";");
       double[] center = new double[values.length];
-      
+
       for (int i = 0; i < values.length; ++i) {
         center[i] = Double.parseDouble(values[i]);
       }
@@ -47,14 +62,19 @@ public class AwesomePasswordChecker {
     br.close();
   }
 
+  /**
+   * 
+   * @param password
+   * @return
+   */
   public int[] maskAff(String password) {
-    int[] maskArray = new int[28]; 
+    int[] maskArray = new int[28];
     int limit = Math.min(password.length(), 28);
-    
+
     for (int i = 0; i < limit; ++i) {
-          char c = password.charAt(i);
+      char c = password.charAt(i);
       switch (c) {
-        case 'e': 
+        case 'e':
         case 's':
         case 'a':
         case 'i':
@@ -64,7 +84,7 @@ public class AwesomePasswordChecker {
         case 'u':
         case 'o':
         case 'l':
-            maskArray[i] = 1;
+          maskArray[i] = 1;
           break;
         case 'E':
         case 'S':
@@ -105,6 +125,11 @@ public class AwesomePasswordChecker {
     return maskArray;
   }
 
+  /**
+   * 
+   * @param password
+   * @return
+   */
   public double getDIstance(String password) {
     int[] maskArray = maskAff(password);
     double minDistance = Double.MAX_VALUE;
@@ -114,6 +139,12 @@ public class AwesomePasswordChecker {
     return minDistance;
   }
 
+  /**
+   * 
+   * @param a
+   * @param b
+   * @return
+   */
   private double euclideanDistance(int[] a, double[] b) {
     double sum = 0;
     for (int i = 0; i < a.length; i++) {
@@ -122,6 +153,11 @@ public class AwesomePasswordChecker {
     return Math.sqrt(sum);
   }
 
+  /**
+   * 
+   * @param input
+   * @return
+   */
   public static String ComputeMD5(String input) {
     byte[] message = input.getBytes();
     int messageLenBytes = message.length;
@@ -141,28 +177,28 @@ public class AwesomePasswordChecker {
     System.arraycopy(lengthBytes, 0, paddedMessage, totalLen - 8, 8);
 
     int[] h = {
-      0x67452301,
-      0xefcdab89,
-      0x98badcfe,
-      0x10325476
+        0x67452301,
+        0xefcdab89,
+        0x98badcfe,
+        0x10325476
     };
 
     int[] k = {
-      0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
-      0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
-      0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa, 0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
-      0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed, 0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
-      0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c, 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
-      0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05, 0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
-      0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
-      0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
+        0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+        0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+        0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa, 0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+        0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed, 0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+        0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c, 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+        0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05, 0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+        0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+        0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
     };
 
     int[] r = {
-      7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
-      5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20,
-      4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
-      6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21
+        7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
+        5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20,
+        4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
+        6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21
     };
 
     for (int i = 0; i < numBlocks; i++) {
